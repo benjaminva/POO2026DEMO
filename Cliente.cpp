113d0089b6fa76f3de7ecc741a9cc15c27f65259
Ng <benjaminva@gmail.com>
2025-03-13 11:47:51 -0600

    Correccion, Agrega archivo Faltanta Cliente.cpp
    Falto agegar archivo faltante Cliente.cpp

---
+++ Cliente.cpp
@@ -0,0 +1,53 @@
+#include "Cliente.h"
+
+// Constructor con por defecto
+// Inicializa los atributos de la clase Cliente con los valores por defecto.
+Cliente::Cliente() : nombre(""), necesidad(""), ubicacion(""), estatus(false) {}
+
+
+// Constructor con lista de inicialización
+// Inicializa los atributos de la clase Cliente con los valores proporcionados.
+Cliente::Cliente(std::string nom, std::string nec, std::string ubi, bool est)
+    : nombre(nom), necesidad(nec), ubicacion(ubi), estatus(est) {}
+
+// Implementación de Getters
+// Devuelve la necesidad del cliente.
+std::string Cliente::getNecesidad() const {
+    return necesidad;
+}
+
+// Devuelve el nombre del cliente.
+std::string Cliente::getNombre() const {
+    return nombre;
+}
+
+// Devuelve la ubicación del cliente.
+std::string Cliente::getUbicacion() const {
+    return ubicacion;
+}
+
+// Devuelve el estado del cliente (activo/inactivo).
+bool Cliente::getEstatus() const {
+    return estatus;
+}
+
+// Implementación de Setters
+// Modifica la necesidad del cliente.
+void Cliente::setNecesidad(std::string nuevaNec) {
+    necesidad = nuevaNec;
+}
+
+// Modifica el nombre del cliente.
+void Cliente::setNombre(std::string nuevoNom) {
+    nombre = nuevoNom;
+}
+
+// Modifica la ubicación del cliente.
+void Cliente::setUbicacion(std::string nuevaUbi) {
+    ubicacion = nuevaUbi;
+}
+
+// Modifica el estado del cliente.
+void Cliente::setEstatus(bool nuevoEst) {
+    estatus = nuevoEst;
+}
